ef293df16c7e870e8227ad3067c4f43aab6aedba
IliaBu <bunamaksi@yandex.ru>
2024-02-04 00:34:18 +0500

    added the function of opening a window with a form for entering a name and password

--- src/main/java/gb/jdk/client/ClientGUI.java
+++ src/main/java/gb/jdk/client/ClientGUI.java
@@ -1,4 +1,79 @@
 package gb.jdk.client;
 
-public class ClientGUI {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ClientGUI extends JFrame {
+
+    private static final int WIDTH = 300;
+    private static final int HEIGHT = 350;
+
+    private final JTextArea log = new JTextArea();
+
+    private final JPanel panelTop = new JPanel(new GridLayout(2,3));
+    private final JTextField tfIPAdress = new JTextField("127.0.0.1");
+    private final JTextField tfPort = new JTextField("8189");
+    private final JTextField tfLogin= new JTextField("ilya_bunakov");
+    private final JPasswordField tfPassword = new JPasswordField("19754825");
+    private final JButton btnLogin = new JButton("Login");
+
+    private final JPanel panelBottom = new JPanel(new BorderLayout());
+    private final JTextField tfMessage = new JTextField();
+    private final JButton btnSend = new JButton("Send");
+
+    public ClientGUI(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Чат клиента");
+        setLocationRelativeTo(null);
+        setLocation(getX() - WIDTH / 2, getY() - HEIGHT / 2);
+        setSize(WIDTH, HEIGHT);
+
+        panelTop.add(tfIPAdress);
+        panelTop.add(tfPort);
+        panelTop.add(tfLogin);
+        panelTop.add(tfPassword);
+        panelTop.add(btnLogin);
+        add(panelTop, BorderLayout.NORTH);
+
+        tfLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        panelBottom.add(tfMessage, BorderLayout.CENTER);
+        panelBottom.add(btnSend, BorderLayout.EAST);
+        add(panelBottom, BorderLayout.SOUTH);
+
+        tfMessage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        btnSend.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        log.setEditable(false);
+        JScrollPane scrollLog = new JScrollPane(log);
+        add(scrollLog);
+
+        setVisible(true);
+    }
+
 }
